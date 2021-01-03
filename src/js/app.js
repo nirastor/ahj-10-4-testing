208896a4f3ebeeec471d17e93925f1a14170475d
Nikita Rastorguev <nirastor@gmail.com>
2021-01-03 18:27:09 +0300

    update app.js (add images to build)

--- src/js/app.js
+++ src/js/app.js
@@ -9,16 +9,16 @@ class App {
     this.message = document.querySelector('.validator-message');
     this.picture = document.querySelector('.validator-pic');
     this.cardsURL = {
-      mir: './src/img/mir.png',
-      diners: './src/img/diners.png',
-      jcb: './src/img/jcb.png',
-      ae: './src/img/ae.png',
-      visa: './src/img/visa.png',
-      maestro: './src/img/maestro.png',
-      mastercard: './src/img/mastercard.png',
-      discover: './src/img/discover.png',
-      union: './src/img/union.png',
-      uek: './src/img/uek.png',
+      mir: '../src/img/mir.png',
+      diners: '../src/img/diners.png',
+      jcb: '../src/img/jcb.png',
+      ae: '../src/img/ae.png',
+      visa: '../src/img/visa.png',
+      maestro: '../src/img/maestro.png',
+      mastercard: '../src/img/mastercard.png',
+      discover: '../src/img/discover.png',
+      union: '../src/img/union.png',
+      uek: '../src/img/uek.png',
     };
     this.initListeners();
   }
